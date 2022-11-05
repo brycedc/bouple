badf58b64551cfed0ecf6b1716c39982e6ec1d05
Bryce <brycedustincollins@gmail.com>
2022-11-05 15:06:22 -0600

    Minor adjustments

--- app/src/main/java/com/example/couplesbudgeting/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/couplesbudgeting/ui/dashboard/DashboardFragment.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.PopupWindow;
 
 import com.example.couplesbudgeting.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -32,10 +33,18 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        // Sets up card navigation
         CardView nav_to_transaction = view.findViewById(R.id.transactions_card);
         CardView nav_to_goals = view.findViewById(R.id.goals_card);
         nav_to_transaction.setOnClickListener(this);
         nav_to_goals.setOnClickListener(this);
+
+        // Sets up popup windows
+        Button add_transaction = view.findViewById(R.id.add_trans_button);
+        Button add_goal = view.findViewById(R.id.add_goal_button);
+        add_transaction.setOnClickListener(this);
+        add_goal.setOnClickListener(this);
         return view;
     }
 
@@ -56,6 +65,14 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
             case R.id.goals_card:
                 navView.setSelectedItemId(R.id.navigation_goals);
                 break;
+            case R.id.add_trans_button:
+
+                break;
+            case R.id.add_goal_button:
+
+                break;
         }
     }
+
+
 }
