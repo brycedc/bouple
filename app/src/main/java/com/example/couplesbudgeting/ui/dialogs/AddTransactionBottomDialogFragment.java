ec56094ae1bac2b345bfbd30b59588a46ed3dd99
Bryce <brycedustincollins@gmail.com>
2022-11-17 10:56:47 -0700

    Added category selection to dialogs

--- app/src/main/java/com/example/couplesbudgeting/ui/dialogs/AddTransactionBottomDialogFragment.java
+++ app/src/main/java/com/example/couplesbudgeting/ui/dialogs/AddTransactionBottomDialogFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 
 import androidx.annotation.Nullable;
@@ -12,6 +14,9 @@ import com.example.couplesbudgeting.R;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class AddTransactionBottomDialogFragment extends BottomSheetDialogFragment implements View.OnClickListener {
+
+    String[] categories = {"Rent", "Utilities", "Entertainment", "Groceries", "Savings", "Transfer"};
+
     public static AddTransactionBottomDialogFragment newInstance() {
         return new AddTransactionBottomDialogFragment();
     }
@@ -25,10 +30,15 @@ public class AddTransactionBottomDialogFragment extends BottomSheetDialogFragmen
         View view = inflater.inflate(R.layout.dialog_add_transaction, container,
                 false);
 
+
+        AutoCompleteTextView autoCompleteText = view.findViewById(R.id.auto_complete_transactions);
+        ArrayAdapter<String> adapterItems = new ArrayAdapter<String>(view.getContext(), R.layout.list_item, categories);
+        autoCompleteText.setAdapter(adapterItems);
+
+
+        // Sets up button
         Button add_goal = view.findViewById(R.id.add_item_popup_button);
         add_goal.setOnClickListener(this);
-
-        // get the views and attach the listener
 
         return view;
 
