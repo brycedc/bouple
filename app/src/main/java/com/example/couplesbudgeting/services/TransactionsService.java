55d73ad3a33d8868f4f1a41b632995043c84397f
Frost77011 <strongjared@gmail.com>
2022-11-28 17:00:33 -0700

    Implemented CreateTransaction in Service and Dialog

--- app/src/main/java/com/example/couplesbudgeting/services/TransactionsService.java
+++ app/src/main/java/com/example/couplesbudgeting/services/TransactionsService.java
@@ -1,10 +1,22 @@
 package com.example.couplesbudgeting.services;
 
 
+import static android.content.ContentValues.TAG;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.example.couplesbudgeting.cache.Cache;
 import com.example.couplesbudgeting.models.Transaction;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Used to communicated with the Firebase Transactions table
@@ -17,8 +29,32 @@ public class TransactionsService {
         db = FirebaseFirestore.getInstance();
     }
 
-    public void createTransaction(String name, String category, Double amount, Date state) {
-        Transaction newTransaction = new Transaction(name, category, amount, state);
+    public void createTransaction(String name, String category, Double amount, Date date) {
+//        Transaction newTransaction = new Transaction(name, category, amount, date);
+        Cache cache = Cache.getInstance();
+
+        Map<String, Object> transaction = new HashMap<>();
+        transaction.put("name", name);
+        transaction.put("category", category);
+        transaction.put("amount", amount);
+        transaction.put("date", date);
+        transaction.put("user_id", cache.getUserId());
+        transaction.put("group_id", cache.getGroupId());
+
+        db.collection("transactions")
+                .add(transaction)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
     }
 
     public Transaction getTransaction() {
--- app/src/main/java/com/example/couplesbudgeting/ui/dialogs/AddTransactionBottomDialogFragment.java
+++ app/src/main/java/com/example/couplesbudgeting/ui/dialogs/AddTransactionBottomDialogFragment.java
@@ -5,16 +5,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 
 import com.example.couplesbudgeting.R;
+import com.example.couplesbudgeting.services.TransactionsService;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import java.util.Date;
 
 public class AddTransactionBottomDialogFragment extends BottomSheetDialogFragment implements View.OnClickListener {
     public static AddTransactionBottomDialogFragment newInstance() {
         return new AddTransactionBottomDialogFragment();
     }
+
+    EditText amount;
+    EditText transactionName;
+    EditText transactionType;
+    EditText date;
 
     @Nullable
     @Override
@@ -30,6 +39,11 @@ public class AddTransactionBottomDialogFragment extends BottomSheetDialogFragmen
 
         // get the views and attach the listener
 
+        amount = (EditText)view.findViewById(R.id.newTransactionAmount);
+        transactionName = (EditText)view.findViewById(R.id.newTransactionName);
+        transactionType = (EditText) view.findViewById(R.id.newTransactionType);
+        date = (EditText) view.findViewById(R.id.newTransactionDate);
+
         return view;
 
     }
@@ -39,6 +53,16 @@ public class AddTransactionBottomDialogFragment extends BottomSheetDialogFragmen
         switch(view.getId()) {
             case R.id.add_item_popup_button:
                 //TODO: Send information from popup to firebase, close popup.
+
+                System.out.println(amount.getText().toString());
+
+                TransactionsService transactionsService = new TransactionsService();
+
+                transactionsService.createTransaction(transactionName.getText().toString(),
+                        transactionType.getText().toString(), Double.parseDouble(amount.getText().toString()),
+                        new Date());
+
+
                 break;
         }
     }
